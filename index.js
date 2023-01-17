ff09982976595d180c401f8527d3db03850f561c
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-01-17 16:09:22 +0300

    go

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(function(err, req, res, next)
   res.render('error');
 });
 
-app.listen(3000, () => console.log(` listening on port ${port}!`));
+var port = 3000;
+app.listen(port, () => console.log(` listening on port ${port}!`));
 
 module.exports = app;
 
