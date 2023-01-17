ab7196e25a83bc6d048b69be5234830f19467fdb
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-01-17 15:55:59 +0300

    GO

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 app.use(function(req, res, next) { next(createError(404)); });
+
 app.use(function(err, req, res, next) 
 {
   res.locals.message = err.message;
@@ -20,4 +23,7 @@ app.use(function(err, req, res, next)
   res.render('error');
 });
 
+app.listen(3000, () => console.log(` listening on port ${port}!`));
+
 module.exports = app;
+
