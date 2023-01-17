95942cc15f65781a3118c87283958d480e0dca23
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-01-17 13:23:36 +0300

    go

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(function(req, res, next) { next(createError(404)); });
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) 
+{
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
