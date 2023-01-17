c66385bcff00f6cef3ae46c62c369a4a362ddc23
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-01-17 12:58:05 +0300

    go

--- app.js
+++ app.js
@@ -2,23 +2,16 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
 app.use(function(req, res, next) { next(createError(404)); });
-
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
