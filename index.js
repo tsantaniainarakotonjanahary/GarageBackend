76adff72ca20c1bee6fa133fff083eec76ffaf03
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-01-25 15:15:54 +0300

    go

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
 
+app.set('view engine', 'html')
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
