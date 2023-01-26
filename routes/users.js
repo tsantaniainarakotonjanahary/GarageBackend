c424b7d55d68fee82712032124b16682510c853c
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-01-26 10:56:22 +0300

    go

--- routes/users.js
+++ routes/users.js
@@ -162,8 +162,10 @@ router.post('/register', async (req, res) => {
     };
 
     await db.collection("client").insertOne(newClient);
+    
+    const { insertedId } = await db.collection("client").insertOne(newClient);
 
-    const token = jwt.sign({ id: newClient._id }, "Tsanta", { expiresIn: 86400 });
+    const token = jwt.sign({ id: insertedId }, "Tsanta", { expiresIn: 86400 });
 
     const verificationLink = `https://garage-backend-sigma.vercel.app/users/verify?token=${token}`;
     
@@ -290,20 +292,16 @@ router.get('/verify', async (req, res) => {
     }
 
     try {
-        //Connexion à MongoDB
         const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority',{ useUnifiedTopology: true });
         await client.connect();
         const db = client.db("Garage");
-
         const decoded = jwt.verify(token, 'Tsanta');
-        //Recherche de l'utilisateur par ID
+        console.log(decoded);
         const user = await db.collection("client").findOne({ _id: new ObjectId(decoded.id) });
         if (!user) {
             return res.status(404).json({ message: 'Utilisateur non trouvé' });
         }
-        //Mise à jour de l'état de l'utilisateur
         await db.collection("client").updateOne({ _id: new ObjectId(decoded.id) }, { $set: { etat: "validate" } });
-        //Redirection avec le token dans l'URL
         res.redirect(`https://your-app.com/home?token=${token}`);
     } catch (err) {
         res.status(400).json({ message: 'Token non valide' });
